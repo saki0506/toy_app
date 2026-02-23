7aa6dd70f77152fca98a3dd62f8b499ec6481ed2
saki0506 <172128609+saki0506@users.noreply.github.com>
2026-02-23 19:56:17 +0900

    Add validation to User and association between User and Micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
-class Micropost < ApplicationRecord
-  belongs_to :user
-  validates :content, length: { maximum: 140 },
-                      presence: true
+class User < ApplicationRecord
+  has_many :microposts
+  validates :name, presence: true
+  validates :email, presence: true
 end
